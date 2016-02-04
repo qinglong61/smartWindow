42d2db67f20c775df688e03d021b9db32155e0e3
qinglun.duan <qinglong61@163.com>
2016-02-04 23:59:38 +0800

    update

--- content.js
+++ content.js
@@ -10,24 +10,42 @@ $(document).ready(function() {
         enabled = item.enabled;
     });
 
-
+    if (checkVideo()) {
+        popover();
+    }
 });
 
 function checkVideo() {
     for (var i = 0; i < supportedUrlList.length; i++) {
         if (window.location.hostname.indexOf(supportedUrlList[i]) != -1) {
             type = supportedUrlList[i];
-            if (type == "youku.com" && $(#player)) {
+            if (type == "youku.com" && $("#player")) {
                 return true;
             }
         }
     }
+    return false;
 }
 
-function popover(response) {
+function popover() {
+    var iconurl = chrome.extension.getURL("icon.png");
     var html = '<a class="sw_video" id="sw_video">' +
-    '<img src="icon.png"/>' +
+    '<img src="' + iconurl + '"/>' +
     '</a>';
+
+    $('#sw_video img').css({
+        "display":"block",
+        "zIndex":1000,
+        "width":25,
+        "height":17,
+        "position":"absolute",
+        "top":ptop,
+        "left":pleft
+    });
+
+    tmp.hover(function(){
+        $(this).css({"cursor":"pointer"})
+    });
 
     $('#sw_video').remove();
     $('body').append(html);
@@ -49,7 +67,6 @@ function fetchVideoInfo()
     if (type == "youku.com") {
         url = $("param [name='movie']").attr("value") + "?" + $("param [name='flashvars']").attr("value");
         console.log(url);
-
     }
     chrome.runtime.sendMessage({
         type:type,
